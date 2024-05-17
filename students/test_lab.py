8537bbfbc3937ac680772c28e00c252d0b808a2b
kz2wd <itrotky@gmail.com>
2024-05-17 11:44:34 +0200

    implemented rsa

--- students/test_lab.py
+++ students/test_lab.py
@@ -1,10 +1,10 @@
 import random
 
-from common import pgcd, euclide_ext, inverse_modulaire
+from common import pgcd, euclide_ext, inverse_modulaire, str_to_int, int_to_str
 
 from common import crible_eras
 
-from rsa import gen_rsa
+import rsa
 
 if __name__ == "__main__":
     # print(pgcd(7398, 2877))
@@ -14,7 +14,21 @@ if __name__ == "__main__":
     # print(crible_eras(50))
 
     random.seed(0)
-    gen_rsa(10)
+    e, d, n = rsa.gen_rsa(512)
+    m = "ceci est le message de la question"
+    c = rsa.enc_rsa(m, e, n)
+    print(e)
+    print(d)
+    print(n)
+
+    print("Public channel [", c, "]")
+
+    decrypted = rsa.dec_rsa(c, d, n)
+    print("Private channel [", decrypted, "]")
+
+
+
+
 
 
 
