a4d62e278e93e7671466c4d4e75ef7ea06bb2f0c
kz2wd <itrotky@gmail.com>
2024-05-17 10:15:26 +0200

    draft key rsa

--- students/test_lab.py
+++ students/test_lab.py
@@ -1,14 +1,21 @@
+import random
 
 from common import pgcd, euclide_ext, inverse_modulaire
 
 from common import crible_eras
+
+from rsa import gen_rsa
 
 if __name__ == "__main__":
     # print(pgcd(7398, 2877))
     # print(euclide_ext(30, 20))
     # print(inverse_modulaire(11, 3))
 
-    print(crible_eras(50))
+    # print(crible_eras(50))
+
+    random.seed(0)
+    gen_rsa(10)
+
 
 
 
